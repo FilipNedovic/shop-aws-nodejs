363851ba83a03338203fb1833764c1dd5025d38a
Filip Nedovic <Filip_Nedovic@epam.com>
2023-03-03 10:31:20 +0100

    added response headers to handle CORS

--- product-service/functions/get-product-by-id/handler.js
+++ product-service/functions/get-product-by-id/handler.js
@@ -5,8 +5,22 @@ export const getProductById = async (event) => {
   const { productId } = event.pathParameters;
   const product = await productService.getProductById(productId);
   const response = product
-    ? { statusCode: 200, body: JSON.stringify(product) }
-    : { statusCode: 404, body: "Product not found" };
+    ? {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(product),
+      }
+    : {
+        statusCode: 404,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: "Product not found",
+      };
 
   return response;
 };
--- product-service/functions/get-product-list/handler.js
+++ product-service/functions/get-product-list/handler.js
@@ -6,6 +6,10 @@ export const getProductList = async () => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify(products),
   };
 };
