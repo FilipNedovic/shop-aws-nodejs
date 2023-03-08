6a8121723e017bc92a55df6ad207c0eaf893251b
Filip Nedovic <Filip_Nedovic@epam.com>
2023-03-08 13:02:26 +0100

    removed unnecessary files

--- fillStocksTable.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "adding test item to to DynamoDb Stocks table"
-aws dynamodb put-item --table-name Stocks --item '{
-  \"product_id\": {
-    \"S\": \"test-uuid\"
-  },
-  \"count\": {
-    \"N\": \"12\"
-  }
-}'
