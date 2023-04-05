3088ac4ac532e716653472b2c084a96db439738c
Filip Nedovic <Filip_Nedovic@epam.com>
2023-04-05 12:23:24 +0200

    updated lambda to send event to SNS the topic on product creation

--- product-service/functions/catalog-batch-process/handler.js
+++ product-service/functions/catalog-batch-process/handler.js
@@ -1,6 +1,6 @@
 "use strict";
-
 import { productService } from "../../services/productService";
+import { snsClient } from "../../../import-service/libs/snsClient";
 
 export const catalogBatchProcess = async (event) => {
   let response = {
@@ -12,18 +12,35 @@ export const catalogBatchProcess = async (event) => {
     messages: [],
   };
 
-  const records = event.Records;
+  const { Records } = event;
 
-  records.map(async (record) => {
+  for (const record of Records) {
     const { body } = record;
+    response.messages.push(JSON.parse(body));
 
     try {
       await productService.createProduct(JSON.parse(body));
-      response.messages.push(JSON.parse(body));
-    } catch (error) {
-      console.error("Error creating product: ", error);
+
+      const params = {
+        Subject: "Product Creation",
+        Message: `Created product: ${JSON.stringify(body)}`,
+        TopicArn: process.env.TOPIC_ARN,
+      };
+
+      snsClient.publish(params, (error, data) => {
+        if (error) {
+          console.error("Error in SNS send", error, error.stack);
+          response.statusCode = 500;
+        }
+        console.log(
+          `Message ${params.Message} sent to the topic ${params.TopicArn}`
+        );
+        console.log("MessageID is " + data.MessageId);
+      });
+    } catch (err) {
+      console.error(err);
     }
-  });
+  }
 
   return response;
 };
